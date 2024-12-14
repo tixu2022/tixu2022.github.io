ec2c35cf3cc6616141329ea1c6ffed6f746e9649
laochailan <239670+laochailan@users.noreply.github.com>
2024-12-14 18:26:50 -0500

    metadata: add on-scene titles

--- scripts/generate_site.py
+++ scripts/generate_site.py
@@ -4,19 +4,20 @@ import glob
 import os
 import shutil
 
+
 def default_title(name):
     return name.replace("_", " ").replace("-", " ").title()
+
 
 def load_gallery(gallery):
     name = os.path.basename(gallery)
     return {
         "type": "gallery",
         "name": name,
-        "title": default_title(name),        
-        "images": [
-            load_image(image) for image in sorted(glob.iglob(f"{gallery}/*"))
-        ]
+        "title": default_title(name),
+        "images": [load_image(image) for image in sorted(glob.iglob(f"{gallery}/*"))],
     }
+
 
 def load_image(image):
     name = os.path.splitext(os.path.basename(image))[0]
@@ -25,8 +26,9 @@ def load_image(image):
         "name": name,
         "title": default_title(name),
         "src": image.removeprefix("static/"),
-        "thumb": f"thumbnails{image.removeprefix("static/images")}"
+        "thumb": f"thumbnails{image.removeprefix("static/images")}",
     }
+
 
 def filter_entry(entries, entry_data):
     entry = next((e for e in entries if e["name"] == entry_data["name"]), None)
@@ -35,16 +37,20 @@ def filter_entry(entries, entry_data):
 
     if "title" in entry_data:
         entry["title"] = entry_data["title"]
-    
+
     if "select" in entry_data:
         if entry["type"] != "gallery":
-            raise RuntimeError(f"'select' attribute found, but corresponding entry {entry} is not a gallery")
-        entry["selected_images"] = [entry["images"][i-1] for i in entry_data["select"]]
+            raise RuntimeError(
+                f"'select' attribute found, but corresponding entry {entry} is not a gallery"
+            )
+        entry["selected_images"] = [
+            entry["images"][i - 1] for i in entry_data["select"]
+        ]
     elif entry["type"] == "gallery":
         entry["selected_images"] = entry["images"][0:3]
 
-    return entry 
-        
+    return entry
+
 
 def load_section(section, section_data):
     entries = [
@@ -54,30 +60,32 @@ def load_section(section, section_data):
 
     if section_data != None and "order" in section_data:
         entries = [filter_entry(entries, data) for data in section_data["order"]]
-    
-    return {
-        "title": os.path.basename(section),
-        "entries": entries
-    }
+
+    return {"title": os.path.basename(section), "entries": entries}
+
 
 def load_sections(metadata):
-    return [load_section(section, section_data) for section, section_data in metadata["sections"].items()]
-    
+    return [
+        load_section(section, section_data)
+        for section, section_data in metadata["sections"].items()
+    ]
+
+
 def copy_static():
     for tree in glob.iglob("static/*"):
-        dest = "public/"+tree.removeprefix("static/")
+        dest = "public/" + tree.removeprefix("static/")
         if os.path.isdir(tree):
             shutil.copytree(tree, dest, dirs_exist_ok=True)
         else:
             shutil.copy(tree, dest)
-    
+
 
 def generate():
     env = Environment(
-        loader = FileSystemLoader("templates"),
-        autoescape = select_autoescape(),
-        trim_blocks = True,
-        lstrip_blocks = True,
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+        lstrip_blocks=True,
     )
 
     with open("metadata.yml") as f:
@@ -88,20 +96,19 @@ def generate():
     copy_static()
 
     navigation = [
-        { "name": "Home", "href": "/index.html" },
-        { "name": "About", "href": "/about.html" },
-        { "name": "IG", "href": "https://www.instagram.com/ti_xu_art/" },
-        { "name": "Mail", "href": "mailto:tixu2024[at]gmail.com" },
+        {"name": "Home", "href": "/index.html"},
+        {"name": "About", "href": "/about.html"},
+        {"name": "IG", "href": "https://www.instagram.com/ti_xu_art/"},
+        {"name": "Mail", "href": "mailto:tixu2024[at]gmail.com"},
     ]
 
     for page in ["index", "about"]:
         templ = env.get_template(f"{page}.html")
-        filtered_navigation = [item for item in navigation if item["href"] != f"/{page}.html"]
+        filtered_navigation = [
+            item for item in navigation if item["href"] != f"/{page}.html"
+        ]
         with open(f"public/{page}.html", "w") as f:
-            f.write(templ.render(
-                sections=sections,
-                navigation=filtered_navigation
-            ))
+            f.write(templ.render(sections=sections, navigation=filtered_navigation))
 
     single = env.get_template("single.html")
     for section in sections:
@@ -111,9 +118,6 @@ def generate():
                 os.makedirs(directory)
             with open(f"{directory}/{entry["name"]}.html", "w") as f:
                 f.write(single.render(entry=entry))
-            
 
 
 generate()
-
-    
--- scripts/optimize_images.py
+++ scripts/optimize_images.py
@@ -1,9 +1,9 @@
-
 import subprocess
 import tempfile
 import os
 import glob
 import shutil
+
 
 def convert(src, dest):
     if dest.endswith("webm"):
@@ -12,13 +12,13 @@ def convert(src, dest):
         subprocess.run(["magick", src, dest])
     return dest
 
+
 def resize(src, dest, width):
     if src.endswith("webm"):
         shutil.copy(src, dest)
     else:
         subprocess.run(["magick", src, "-resize", str(width), dest])
-    
-    
+
 
 trial_exts = {
     ".png": [".webp"],
@@ -27,14 +27,21 @@ trial_exts = {
     ".gif": [".webm"],
 }
 
+
 def convert_to_smallest(src, dest, tmpdir):
     basename = os.path.basename(src)
     root, ext = os.path.splitext(basename)
 
-    if any(os.path.isfile(dest + root + target_ext) for target_ext in trial_exts[ext.lower()]):
+    if any(
+        os.path.isfile(dest + root + target_ext)
+        for target_ext in trial_exts[ext.lower()]
+    ):
         print(f"target for {src} already exists, skipping...")
         return
-    trial_files = [convert(src, tmpdir + root + target_ext) for target_ext in trial_exts[ext.lower()]]
+    trial_files = [
+        convert(src, tmpdir + root + target_ext)
+        for target_ext in trial_exts[ext.lower()]
+    ]
     trial_files += [src]
 
     trial_sizes = [os.path.getsize(file) for file in trial_files]
@@ -49,13 +56,17 @@ def convert_to_smallest(src, dest, tmpdir):
 def optimize_directory(src, dest_prefix):
     with tempfile.TemporaryDirectory() as tmp:
         for file in glob.iglob(src + "/**/*.*", recursive=True):
-            os.makedirs(dest_prefix+os.path.dirname(file), exist_ok=True)
-            convert_to_smallest(file, dest_prefix+os.path.dirname(file) + "/", tmp + "/")
+            os.makedirs(dest_prefix + os.path.dirname(file), exist_ok=True)
+            convert_to_smallest(
+                file, dest_prefix + os.path.dirname(file) + "/", tmp + "/"
+            )
+
 
 def resize_directory(src, dest):
-    for file in glob.iglob(src+"/**/*.*", recursive=True):
-        os.makedirs(os.path.dirname(dest+ file.removeprefix(src)), exist_ok=True)
+    for file in glob.iglob(src + "/**/*.*", recursive=True):
+        os.makedirs(os.path.dirname(dest + file.removeprefix(src)), exist_ok=True)
         resize(file, dest + file.removeprefix(src), 700)
+
 
 optimize_directory("images", "static/")
 resize_directory("static/images", "static/thumbnails/")
